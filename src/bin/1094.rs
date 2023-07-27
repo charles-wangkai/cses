4c3732f7b484e945b35a0c987bdb25721fc85758
Kai Wang <charles.wangkai@gmail.com>
2023-07-27 19:28:33 +0800

    Increasing Array

---
+++ src/bin/1094.rs
@@ -0,0 +1,30 @@
+use std::io::{stdin, BufRead, BufReader};
+
+fn main() {
+    let mut br = BufReader::new(stdin());
+
+    let mut line = String::new();
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let n = split.next().unwrap().parse().unwrap();
+    let mut line = String::new();
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let mut x = Vec::new();
+    for _ in 0..n {
+        x.push(split.next().unwrap().parse().unwrap());
+    }
+
+    println!("{}", solve(x));
+}
+
+fn solve(x: Vec<i32>) -> i64 {
+    let mut result = 0;
+    let mut max = 0;
+    for value in x {
+        max = max.max(value);
+        result += (max - value) as i64;
+    }
+
+    result
+}
