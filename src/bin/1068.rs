10a9e98e49fbbb6741d934361a866545a74f6ce5
Kai Wang <charles.wangkai@gmail.com>
2023-07-27 14:51:17 +0800

    Weird Algorithm

---
+++ src/bin/1068.rs
@@ -0,0 +1,35 @@
+use std::io::{stdin, BufRead, BufReader};
+
+fn main() {
+    let mut br = BufReader::new(stdin());
+    let mut line = String::new();
+
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let n = split.next().unwrap().parse().unwrap();
+
+    println!("{}", solve(n));
+}
+
+fn solve(n: i32) -> String {
+    let mut result = Vec::new();
+    let mut current = n as i64;
+    loop {
+        result.push(current);
+        if current == 1 {
+            break;
+        }
+
+        current = if current % 2 == 0 {
+            current / 2
+        } else {
+            current * 3 + 1
+        }
+    }
+
+    result
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join(" ")
+}
