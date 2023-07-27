fc211823b18c2d3d8e2a2a0416605002e946f0be
Kai Wang <charles.wangkai@gmail.com>
2023-07-27 14:15:12 +0800

    A + B

---
+++ src/bin/A.rs
@@ -0,0 +1,19 @@
+#![allow(non_snake_case)]
+
+use std::io::{stdin, BufRead, BufReader};
+
+fn main() {
+    let mut br = BufReader::new(stdin());
+    let mut line = String::new();
+
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let A = split.next().unwrap().parse().unwrap();
+    let B = split.next().unwrap().parse().unwrap();
+
+    println!("{}", solve(A, B));
+}
+
+fn solve(A: i32, B: i32) -> i32 {
+    A + B
+}
