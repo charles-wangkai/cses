e2a43e5f39a92a92ac645ff16e4bc87fe5440e17
Kai Wang <charles.wangkai@gmail.com>
2023-07-27 17:29:27 +0800

    Weird Algorithm

--- src/bin/1068.rs
+++ src/bin/1068.rs
@@ -2,8 +2,8 @@ use std::io::{stdin, BufRead, BufReader};
 
 fn main() {
     let mut br = BufReader::new(stdin());
-    let mut line = String::new();
 
+    let mut line = String::new();
     br.read_line(&mut line).unwrap();
     let mut split = line.split_whitespace();
     let n = split.next().unwrap().parse().unwrap();
