1965eb4403bab00466a8156dfde3f20eef3f5deb
Kai Wang <charles.wangkai@gmail.com>
2023-07-28 09:34:12 +0800

    Trailing Zeros

---
+++ src/bin/1618.rs
@@ -0,0 +1,20 @@
+use std::io::{stdin, BufRead, BufReader};
+
+fn main() {
+    let mut br = BufReader::new(stdin());
+
+    let mut line = String::new();
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let n = split.next().unwrap().parse().unwrap();
+
+    println!("{}", solve(n));
+}
+
+fn solve(n: i32) -> i32 {
+    if n == 0 {
+        0
+    } else {
+        n / 5 + solve(n / 5)
+    }
+}
