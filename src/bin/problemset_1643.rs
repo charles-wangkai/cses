737f6fa2f10aa74b6a1ea1de94dfbaf187f3f9c1
Kai Wang <charles.wangkai@gmail.com>
2023-07-31 17:44:34 +0800

    Maximum Subarray Sum

---
+++ src/bin/problemset_1643.rs
@@ -0,0 +1,31 @@
+use std::io::{stdin, BufRead, BufReader};
+
+fn main() {
+    let mut br = BufReader::new(stdin());
+
+    let mut line = String::new();
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let n = split.next().unwrap().parse().unwrap();
+    let mut line = String::new();
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let mut x = Vec::new();
+    for _ in 0..n {
+        x.push(split.next().unwrap().parse().unwrap());
+    }
+
+    println!("{}", solve(&x));
+}
+
+fn solve(x: &[i32]) -> i64 {
+    let mut result = i64::MIN;
+    let mut sum = 0;
+    for &xi in x {
+        sum += xi as i64;
+        result = result.max(sum);
+        sum = sum.max(0);
+    }
+
+    result
+}
