61f0f72d9e4cecc5ba8e802416243008b294e2c2
Kai Wang <charles.wangkai@gmail.com>
2023-08-02 22:54:47 +0800

    Subarray Sums I

---
+++ src/bin/problemset_1660.rs
@@ -0,0 +1,44 @@
+use std::{
+    collections::HashMap,
+    io::{stdin, BufRead, BufReader},
+};
+
+fn main() {
+    let mut br = BufReader::new(stdin());
+
+    let mut line = String::new();
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let n = split.next().unwrap().parse().unwrap();
+    let x = split.next().unwrap().parse().unwrap();
+    let mut line = String::new();
+    br.read_line(&mut line).unwrap();
+    let mut split = line.split_whitespace();
+    let mut a = Vec::new();
+    for _ in 0..n {
+        a.push(split.next().unwrap().parse().unwrap());
+    }
+
+    println!("{}", solve(&a, x));
+}
+
+fn solve(a: &[i32], x: i32) -> i64 {
+    let mut result = 0;
+    let mut prefix_sum_to_count = HashMap::from([(0, 1)]);
+    let mut prefix_sum = 0;
+    for &ai in a {
+        prefix_sum += ai as i64;
+
+        result += prefix_sum_to_count
+            .get(&(prefix_sum - (x as i64)))
+            .copied()
+            .unwrap_or(0);
+
+        prefix_sum_to_count
+            .entry(prefix_sum)
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    }
+
+    result
+}
