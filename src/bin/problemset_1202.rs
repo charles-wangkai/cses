b7d62dab43c942db16d20cfad94185a7d0c7fce0
Kai Wang <charles.wangkai@gmail.com>
2026-01-29 16:20:51 +0800

    Investigation

--- src/bin/problemset_1202.rs
+++ src/bin/problemset_1202.rs
@@ -4,7 +4,9 @@ use std::{
     io::{stdin, BufRead, BufReader},
 };
 
-const MODULUS: i32 = 1_000_000_007;
+const MOD_INT: ModInt = ModInt {
+    modulus: 1_000_000_007,
+};
 
 fn main() {
     let mut br = BufReader::new(stdin());
@@ -60,7 +62,7 @@ fn solve(n: usize, a: &[usize], b: &[usize], c: &[i32]) -> String {
                     }
                     Ordering::Equal => {
                         route_nums[b[edge] - 1] =
-                            add_mod(route_nums[b[edge] - 1], route_nums[node]);
+                            MOD_INT.add_mod(route_nums[b[edge] - 1], route_nums[node]);
                         min_flight_nums[b[edge] - 1] =
                             min_flight_nums[b[edge] - 1].min(min_flight_nums[node] + 1);
                         max_flight_nums[b[edge] - 1] =
@@ -81,6 +83,40 @@ fn solve(n: usize, a: &[usize], b: &[usize], c: &[i32]) -> String {
     )
 }
 
-fn add_mod(x: i32, y: i32) -> i32 {
-    (x + y).rem_euclid(MODULUS)
+struct ModInt {
+    modulus: i32,
+}
+
+#[allow(dead_code)]
+impl ModInt {
+    fn modulo(&self, x: i64) -> i32 {
+        x.rem_euclid(self.modulus as i64) as i32
+    }
+
+    fn mod_inv(&self, x: i32) -> i32 {
+        self.pow_mod(x, (self.modulus - 2) as i64)
+    }
+
+    fn add_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x + y) as i64)
+    }
+
+    fn multiply_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x as i64) * (y as i64))
+    }
+
+    fn divide_mod(&self, x: i32, y: i32) -> i32 {
+        self.multiply_mod(x, self.mod_inv(y))
+    }
+
+    fn pow_mod(&self, base: i32, exponent: i64) -> i32 {
+        if exponent == 0 {
+            return 1;
+        }
+
+        self.multiply_mod(
+            if exponent % 2 == 0 { 1 } else { base },
+            self.pow_mod(self.multiply_mod(base, base), exponent / 2),
+        )
+    }
 }
