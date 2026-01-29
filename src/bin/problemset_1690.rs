a2f5316a3a99ae22d9c68d187669af26b6c83fee
Kai Wang <charles.wangkai@gmail.com>
2026-01-29 16:40:23 +0800

    Hamiltonian Flights

--- src/bin/problemset_1690.rs
+++ src/bin/problemset_1690.rs
@@ -1,6 +1,8 @@
 use std::io::{stdin, BufRead, BufReader};
 
-const MODULUS: i32 = 1_000_000_007;
+const MOD_INT: ModInt = ModInt {
+    modulus: 1_000_000_007,
+};
 
 fn main() {
     let mut br = BufReader::new(stdin());
@@ -37,7 +39,7 @@ fn solve(n: usize, a: &[usize], b: &[usize]) -> i32 {
                 for &adj in adj_vecs_node {
                     if ((mask >> adj) & 1) == 0 {
                         let next_mask = mask | (1 << adj);
-                        dp[next_mask][adj] = add_mod(dp[next_mask][adj], dp[mask][node]);
+                        dp[next_mask][adj] = MOD_INT.add_mod(dp[next_mask][adj], dp[mask][node]);
                     }
                 }
             }
@@ -47,6 +49,40 @@ fn solve(n: usize, a: &[usize], b: &[usize]) -> i32 {
     dp[(1 << n) - 1][n - 1]
 }
 
-fn add_mod(x: i32, y: i32) -> i32 {
-    (x + y).rem_euclid(MODULUS)
+struct ModInt {
+    modulus: i32,
+}
+
+#[allow(dead_code)]
+impl ModInt {
+    fn modulo(&self, x: i64) -> i32 {
+        x.rem_euclid(self.modulus as i64) as i32
+    }
+
+    fn mod_inv(&self, x: i32) -> i32 {
+        self.pow_mod(x, (self.modulus - 2) as i64)
+    }
+
+    fn add_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x + y) as i64)
+    }
+
+    fn multiply_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x as i64) * (y as i64))
+    }
+
+    fn divide_mod(&self, x: i32, y: i32) -> i32 {
+        self.multiply_mod(x, self.mod_inv(y))
+    }
+
+    fn pow_mod(&self, base: i32, exponent: i64) -> i32 {
+        if exponent == 0 {
+            return 1;
+        }
+
+        self.multiply_mod(
+            if exponent % 2 == 0 { 1 } else { base },
+            self.pow_mod(self.multiply_mod(base, base), exponent / 2),
+        )
+    }
 }
