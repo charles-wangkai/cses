0d68baf24b03b7e55cf3379c3405c7266a6b3ebf
Kai Wang <charles.wangkai@gmail.com>
2026-01-29 16:25:33 +0800

    Bit Strings

--- src/bin/problemset_1617.rs
+++ src/bin/problemset_1617.rs
@@ -1,6 +1,8 @@
 use std::io::{stdin, BufRead, BufReader};
 
-const MODULUS: i32 = 1_000_000_007;
+const MOD_INT: ModInt = ModInt {
+    modulus: 1_000_000_007,
+};
 
 fn main() {
     let mut br = BufReader::new(stdin());
@@ -14,9 +16,43 @@ fn main() {
 }
 
 fn solve(n: i32) -> i32 {
-    (0..n).fold(1, |acc, _| multiply_mod(acc, 2))
+    (0..n).fold(1, |acc, _| MOD_INT.multiply_mod(acc, 2))
 }
 
-fn multiply_mod(x: i32, y: i32) -> i32 {
-    ((x as i64) * (y as i64) % (MODULUS as i64)) as i32
+struct ModInt {
+    modulus: i32,
+}
+
+#[allow(dead_code)]
+impl ModInt {
+    fn modulo(&self, x: i64) -> i32 {
+        x.rem_euclid(self.modulus as i64) as i32
+    }
+
+    fn mod_inv(&self, x: i32) -> i32 {
+        self.pow_mod(x, (self.modulus - 2) as i64)
+    }
+
+    fn add_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x + y) as i64)
+    }
+
+    fn multiply_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x as i64) * (y as i64))
+    }
+
+    fn divide_mod(&self, x: i32, y: i32) -> i32 {
+        self.multiply_mod(x, self.mod_inv(y))
+    }
+
+    fn pow_mod(&self, base: i32, exponent: i64) -> i32 {
+        if exponent == 0 {
+            return 1;
+        }
+
+        self.multiply_mod(
+            if exponent % 2 == 0 { 1 } else { base },
+            self.pow_mod(self.multiply_mod(base, base), exponent / 2),
+        )
+    }
 }
