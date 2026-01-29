5f07f0b2256c96aede334c6b39f24aaf6823bd7a
Kai Wang <charles.wangkai@gmail.com>
2026-01-29 17:03:37 +0800

    Counting Towers

--- src/bin/problemset_2413.rs
+++ src/bin/problemset_2413.rs
@@ -1,7 +1,9 @@
 use std::io::{stdin, BufRead, BufReader};
 
 const LIMIT: usize = 1_000_000;
-const MODULUS: i32 = 1_000_000_007;
+const MOD_INT: ModInt = ModInt {
+    modulus: 1_000_000_007,
+};
 
 fn main() {
     let mut dp = vec![0; LIMIT + 1];
@@ -29,10 +31,10 @@ fn precompute(dp: &mut [i32]) {
     let mut sum = 1;
     let mut progression_sum = 1;
     for dpi in dp.iter_mut().skip(1) {
-        *dpi = add_mod(sum, progression_sum);
+        *dpi = MOD_INT.add_mod(sum, progression_sum);
 
-        sum = add_mod(sum, *dpi);
-        progression_sum = add_mod(multiply_mod(progression_sum, 3), *dpi);
+        sum = MOD_INT.add_mod(sum, *dpi);
+        progression_sum = MOD_INT.add_mod(MOD_INT.multiply_mod(progression_sum, 3), *dpi);
     }
 }
 
@@ -40,10 +42,40 @@ fn solve(dp: &[i32], n: usize) -> i32 {
     dp[n]
 }
 
-fn add_mod(x: i32, y: i32) -> i32 {
-    (x + y).rem_euclid(MODULUS)
+struct ModInt {
+    modulus: i32,
 }
 
-fn multiply_mod(x: i32, y: i32) -> i32 {
-    ((x as i64) * (y as i64)).rem_euclid(MODULUS as i64) as i32
+#[allow(dead_code)]
+impl ModInt {
+    fn modulo(&self, x: i64) -> i32 {
+        x.rem_euclid(self.modulus as i64) as i32
+    }
+
+    fn mod_inv(&self, x: i32) -> i32 {
+        self.pow_mod(x, (self.modulus - 2) as i64)
+    }
+
+    fn add_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x + y) as i64)
+    }
+
+    fn multiply_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x as i64) * (y as i64))
+    }
+
+    fn divide_mod(&self, x: i32, y: i32) -> i32 {
+        self.multiply_mod(x, self.mod_inv(y))
+    }
+
+    fn pow_mod(&self, base: i32, exponent: i64) -> i32 {
+        if exponent == 0 {
+            return 1;
+        }
+
+        self.multiply_mod(
+            if exponent % 2 == 0 { 1 } else { base },
+            self.pow_mod(self.multiply_mod(base, base), exponent / 2),
+        )
+    }
 }
