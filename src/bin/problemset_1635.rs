bcd913ed87f5cd3e296a21114d370bf1ff66ea46
Kai Wang <charles.wangkai@gmail.com>
2026-01-29 16:30:18 +0800

    Coin Combinations I

--- src/bin/problemset_1635.rs
+++ src/bin/problemset_1635.rs
@@ -1,6 +1,8 @@
 use std::io::{stdin, BufRead, BufReader};
 
-const MODULUS: i32 = 1_000_000_007;
+const MOD_INT: ModInt = ModInt {
+    modulus: 1_000_000_007,
+};
 
 fn main() {
     let mut br = BufReader::new(stdin());
@@ -27,7 +29,7 @@ fn solve(c: &[i32], x: i32) -> i32 {
     for i in 1..dp.len() {
         for &ci in c {
             if ci <= i as i32 {
-                dp[i] = add_mod(dp[i], dp[i - (ci as usize)]);
+                dp[i] = MOD_INT.add_mod(dp[i], dp[i - (ci as usize)]);
             }
         }
     }
@@ -35,6 +37,40 @@ fn solve(c: &[i32], x: i32) -> i32 {
     dp.last().copied().unwrap()
 }
 
-fn add_mod(x: i32, y: i32) -> i32 {
-    (x + y).rem_euclid(MODULUS)
+struct ModInt {
+    modulus: i32,
+}
+
+#[allow(dead_code)]
+impl ModInt {
+    fn modulo(&self, x: i64) -> i32 {
+        x.rem_euclid(self.modulus as i64) as i32
+    }
+
+    fn mod_inv(&self, x: i32) -> i32 {
+        self.pow_mod(x, (self.modulus - 2) as i64)
+    }
+
+    fn add_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x + y) as i64)
+    }
+
+    fn multiply_mod(&self, x: i32, y: i32) -> i32 {
+        self.modulo((x as i64) * (y as i64))
+    }
+
+    fn divide_mod(&self, x: i32, y: i32) -> i32 {
+        self.multiply_mod(x, self.mod_inv(y))
+    }
+
+    fn pow_mod(&self, base: i32, exponent: i64) -> i32 {
+        if exponent == 0 {
+            return 1;
+        }
+
+        self.multiply_mod(
+            if exponent % 2 == 0 { 1 } else { base },
+            self.pow_mod(self.multiply_mod(base, base), exponent / 2),
+        )
+    }
 }
