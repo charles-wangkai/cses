43ae83482b5b1253c7b4ddf884a2cf5ef41a68e0
Kai Wang <charles.wangkai@gmail.com>
2026-01-30 23:04:02 +0800

    Giant Pizza

--- src/bin/problemset_1684.rs
+++ src/bin/problemset_1684.rs
@@ -1,9 +1,4 @@
-// https://cp-algorithms.com/graph/2SAT.html
-
-use std::{
-    cmp::Ordering,
-    io::{stdin, BufRead, BufReader},
-};
+use std::io::{stdin, BufRead, BufReader};
 
 fn main() {
     let mut br = BufReader::new(stdin());
@@ -37,81 +32,138 @@ fn solve(
     topping2s: &[usize],
     m: usize,
 ) -> String {
-    let mut adj_vecs = vec![Vec::new(); m * 2];
+    let mut two_sat = TwoSat::new(m);
     for i in 0..symbol1s.len() {
-        let index1 = (topping1s[i] - 1) * 2 + (if symbol1s[i] == '+' { 0 } else { 1 });
-        let index2 = (topping2s[i] - 1) * 2 + (if symbol2s[i] == '+' { 0 } else { 1 });
-
-        adj_vecs[index1 ^ 1].push(index2);
-        adj_vecs[index2 ^ 1].push(index1);
+        two_sat.add_clause(
+            topping1s[i] - 1,
+            symbol1s[i] == '+',
+            topping2s[i] - 1,
+            symbol2s[i] == '+',
+        );
     }
 
-    let mut sorted_nodes = Vec::new();
-    let mut visited = vec![false; m * 2];
-    for i in 0..visited.len() {
-        if !visited[i] {
-            search1(&adj_vecs, &mut sorted_nodes, &mut visited, i);
-        }
-    }
-    sorted_nodes.reverse();
-
-    let mut reversed_adj_vecs = vec![Vec::new(); m * 2];
-    for i in 0..symbol1s.len() {
-        let index1 = (topping1s[i] - 1) * 2 + (if symbol1s[i] == '+' { 0 } else { 1 });
-        let index2 = (topping2s[i] - 1) * 2 + (if symbol2s[i] == '+' { 0 } else { 1 });
-
-        reversed_adj_vecs[index2].push(index1 ^ 1);
-        reversed_adj_vecs[index1].push(index2 ^ 1);
-    }
-
-    let mut k = 0;
-    let mut components = vec![0; m * 2];
-    for node in sorted_nodes {
-        if components[node] == 0 {
-            k += 1;
-            search2(&reversed_adj_vecs, &mut components, node, k);
-        }
-    }
-
-    let mut result = vec!['\0'; m];
-    for i in 0..m {
-        result[i] = match components[i * 2].cmp(&components[i * 2 + 1]) {
-            Ordering::Less => '-',
-            Ordering::Equal => return String::from("IMPOSSIBLE"),
-            Ordering::Greater => '+',
-        };
-    }
-
-    result
-        .iter()
-        .map(|x| x.to_string())
-        .collect::<Vec<_>>()
-        .join(" ")
-}
-
-fn search2(reversed_adj_vecs: &[Vec<usize>], components: &mut [usize], node: usize, k: usize) {
-    components[node] = k;
-
-    for &adj in &reversed_adj_vecs[node] {
-        if components[adj] == 0 {
-            search2(reversed_adj_vecs, components, adj, k);
-        }
+    match two_sat.find_assignment() {
+        Some(assignment) => assignment
+            .iter()
+            .map(|&x| if x { "+" } else { "-" })
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" "),
+        None => "IMPOSSIBLE".to_string(),
     }
 }
 
-fn search1(
-    adj_vecs: &[Vec<usize>],
-    sorted_nodes: &mut Vec<usize>,
-    visited: &mut [bool],
-    node: usize,
-) {
-    visited[node] = true;
+struct TwoSat {
+    n: usize,
+    scc: Scc,
+}
 
-    for &adj in &adj_vecs[node] {
-        if !visited[adj] {
-            search1(adj_vecs, sorted_nodes, visited, adj);
+#[allow(dead_code)]
+impl TwoSat {
+    fn new(n: usize) -> Self {
+        Self {
+            n,
+            scc: Scc::new(2 * n),
         }
     }
 
-    sorted_nodes.push(node);
+    fn add_clause(&mut self, i: usize, f: bool, j: usize, g: bool) {
+        self.scc.add_edge(
+            2 * i + (if f { 0 } else { 1 }),
+            2 * j + (if g { 1 } else { 0 }),
+        );
+        self.scc.add_edge(
+            2 * j + (if g { 0 } else { 1 }),
+            2 * i + (if f { 1 } else { 0 }),
+        );
+    }
+
+    fn find_assignment(&self) -> Option<Vec<bool>> {
+        let components = self.scc.build_components();
+
+        let mut assignment = vec![false; self.n];
+        for i in 0..assignment.len() {
+            if components[2 * i] == components[2 * i + 1] {
+                return None;
+            }
+
+            assignment[i] = components[2 * i] < components[2 * i + 1];
+        }
+
+        Some(assignment)
+    }
+}
+
+struct Scc {
+    adj_vecs: Vec<Vec<usize>>,
+    reversed_adj_vecs: Vec<Vec<usize>>,
+}
+
+#[allow(dead_code)]
+impl Scc {
+    fn new(n: usize) -> Self {
+        Self {
+            adj_vecs: vec![Vec::new(); n],
+            reversed_adj_vecs: vec![Vec::new(); n],
+        }
+    }
+
+    fn add_edge(&mut self, from: usize, to: usize) {
+        self.adj_vecs[from].push(to);
+        self.reversed_adj_vecs[to].push(from);
+    }
+
+    fn topological_sort(&self) -> Vec<usize> {
+        let n = self.adj_vecs.len();
+
+        let mut sorted = Vec::new();
+        let mut visited = vec![false; n];
+        for i in 0..n {
+            if !visited[i] {
+                self.search1(&mut sorted, &mut visited, i);
+            }
+        }
+        sorted.reverse();
+
+        sorted
+    }
+
+    fn search1(&self, sorted: &mut Vec<usize>, visited: &mut [bool], node: usize) {
+        visited[node] = true;
+
+        for &adj in &self.adj_vecs[node] {
+            if !visited[adj] {
+                self.search1(sorted, visited, adj);
+            }
+        }
+
+        sorted.push(node);
+    }
+
+    fn build_components(&self) -> Vec<usize> {
+        let n = self.adj_vecs.len();
+
+        let sorted = self.topological_sort();
+
+        let mut components = vec![usize::MAX; n];
+        let mut component = 0;
+        for node in sorted {
+            if components[node] == usize::MAX {
+                self.search2(&mut components, component, node);
+                component += 1;
+            }
+        }
+
+        components
+    }
+
+    fn search2(&self, components: &mut [usize], component: usize, node: usize) {
+        components[node] = component;
+
+        for &adj in &self.reversed_adj_vecs[node] {
+            if components[adj] == usize::MAX {
+                self.search2(components, component, adj);
+            }
+        }
+    }
 }
