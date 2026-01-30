6f24f37833fb10dd32e04ce116eb3d882c7c8511
Kai Wang <charles.wangkai@gmail.com>
2026-01-30 10:36:50 +0800

    Forest Queries II

--- src/bin/problemset_1739.rs
+++ src/bin/problemset_1739.rs
@@ -27,12 +27,11 @@ fn main() {
 fn solve(squares: &mut [Vec<u8>], queries: &[String]) -> String {
     let n = squares.len();
 
-    let mut binary_indexed_tree =
-        vec![vec![0; (1 << (n.ilog2() + 1)) + 1]; (1 << (n.ilog2() + 1)) + 1];
+    let mut fenwick_tree = FenwickTree::new(n, n);
     for x in 1..=n {
         for y in 1..=n {
             if squares[x - 1][y - 1] == b'*' {
-                update(&mut binary_indexed_tree, x, y, 1);
+                fenwick_tree.add(x, y, 1);
             }
         }
     }
@@ -45,10 +44,10 @@ fn solve(squares: &mut [Vec<u8>], queries: &[String]) -> String {
             let c = parts[2].parse().unwrap();
 
             if squares[r - 1][c - 1] == b'*' {
-                update(&mut binary_indexed_tree, r, c, -1);
+                fenwick_tree.add(r, c, -1);
                 squares[r - 1][c - 1] = b'.';
             } else {
-                update(&mut binary_indexed_tree, r, c, 1);
+                fenwick_tree.add(r, c, 1);
                 squares[r - 1][c - 1] = b'*';
             }
         } else {
@@ -58,10 +57,10 @@ fn solve(squares: &mut [Vec<u8>], queries: &[String]) -> String {
             let c2 = parts[4].parse().unwrap();
 
             result.push(
-                compute_prefix_sum(&binary_indexed_tree, r2, c2)
-                    - compute_prefix_sum(&binary_indexed_tree, r1 - 1, c2)
-                    - compute_prefix_sum(&binary_indexed_tree, r2, c1 - 1)
-                    + compute_prefix_sum(&binary_indexed_tree, r1 - 1, c1 - 1),
+                fenwick_tree.compute_prefix_sum(r2, c2)
+                    - fenwick_tree.compute_prefix_sum(r1 - 1, c2)
+                    - fenwick_tree.compute_prefix_sum(r2, c1 - 1)
+                    + fenwick_tree.compute_prefix_sum(r1 - 1, c1 - 1),
             );
         }
     }
@@ -73,36 +72,49 @@ fn solve(squares: &mut [Vec<u8>], queries: &[String]) -> String {
         .join("\n")
 }
 
-fn compute_prefix_sum(binary_indexed_tree: &[Vec<i32>], mut x: usize, y: usize) -> i32 {
-    let mut result = 0;
-    while x != 0 {
-        result += compute_prefix_sum_y(binary_indexed_tree, x, y);
-        x -= ((x as i32) & -(x as i32)) as usize;
-    }
-
-    result
+struct FenwickTree {
+    a: Vec<Vec<i32>>,
 }
 
-fn compute_prefix_sum_y(binary_indexed_tree: &[Vec<i32>], x: usize, mut y: usize) -> i32 {
-    let mut result = 0;
-    while y != 0 {
-        result += binary_indexed_tree[x][y];
-        y -= ((y as i32) & -(y as i32)) as usize;
+#[allow(dead_code)]
+impl FenwickTree {
+    fn new(size1: usize, size2: usize) -> Self {
+        Self {
+            a: vec![vec![0; (1 << (size2.ilog2() + 1)) + 1]; (1 << (size1.ilog2() + 1)) + 1],
+        }
     }
 
-    result
-}
-
-fn update(binary_indexed_tree: &mut [Vec<i32>], mut x: usize, y: usize, delta: i32) {
-    while x < binary_indexed_tree.len() {
-        update_y(binary_indexed_tree, x, y, delta);
-        x += ((x as i32) & -(x as i32)) as usize;
+    fn add(&mut self, mut x: usize, y: usize, delta: i32) {
+        while x < self.a.len() {
+            self.add_y(x, y, delta);
+            x += ((x as i32) & -(x as i32)) as usize;
+        }
     }
-}
 
-fn update_y(binary_indexed_tree: &mut [Vec<i32>], x: usize, mut y: usize, delta: i32) {
-    while y < binary_indexed_tree[x].len() {
-        binary_indexed_tree[x][y] += delta;
-        y += ((y as i32) & -(y as i32)) as usize;
+    fn add_y(&mut self, x: usize, mut y: usize, delta: i32) {
+        while y < self.a.len() {
+            self.a[x][y] += delta;
+            y += ((y as i32) & -(y as i32)) as usize;
+        }
+    }
+
+    fn compute_prefix_sum(&self, mut x: usize, y: usize) -> i32 {
+        let mut result = 0;
+        while x != 0 {
+            result += self.compute_prefix_sum_y(x, y);
+            x -= ((x as i32) & -(x as i32)) as usize;
+        }
+
+        result
+    }
+
+    fn compute_prefix_sum_y(&self, x: usize, mut y: usize) -> i32 {
+        let mut result = 0;
+        while y != 0 {
+            result += self.a[x][y];
+            y -= ((y as i32) & -(y as i32)) as usize;
+        }
+
+        result
     }
 }
