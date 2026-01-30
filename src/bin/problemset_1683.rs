50f3976bf1c0aa4df9cf4e126b686fe982216321
Kai Wang <charles.wangkai@gmail.com>
2026-01-30 20:20:31 +0800

    Planets and Kingdoms

--- src/bin/problemset_1683.rs
+++ src/bin/problemset_1683.rs
@@ -1,5 +1,3 @@
-// https://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
-
 use std::io::{stdin, BufRead, BufReader};
 
 fn main() {
@@ -24,68 +22,95 @@ fn main() {
 }
 
 fn solve(n: usize, a: &[usize], b: &[usize]) -> String {
-    let mut adj_vecs = vec![Vec::new(); n];
+    let mut scc = Scc::new(n);
     for i in 0..a.len() {
-        adj_vecs[a[i] - 1].push(b[i] - 1);
+        scc.add_edge(a[i] - 1, b[i] - 1);
     }
 
-    let mut sorted_nodes = Vec::new();
-    let mut visited = vec![false; n];
-    for i in 0..visited.len() {
-        if !visited[i] {
-            search1(&adj_vecs, &mut sorted_nodes, &mut visited, i);
-        }
-    }
-    sorted_nodes.reverse();
-
-    let mut reversed_adj_vecs = vec![Vec::new(); n];
-    for i in 0..a.len() {
-        reversed_adj_vecs[b[i] - 1].push(a[i] - 1);
-    }
-
-    let mut k = 0;
-    let mut kingdoms = vec![0; n];
-    for node in sorted_nodes {
-        if kingdoms[node] == 0 {
-            k += 1;
-            search2(&reversed_adj_vecs, &mut kingdoms, node, k);
-        }
-    }
+    let components = scc.build_components();
 
     format!(
         "{}\n{}",
-        k,
-        kingdoms
+        components.iter().max().unwrap() + 1,
+        components
             .iter()
+            .map(|component| component + 1)
             .map(|x| x.to_string())
             .collect::<Vec<_>>()
             .join(" ")
     )
 }
 
-fn search2(reversed_adj_vecs: &[Vec<usize>], kingdoms: &mut [usize], node: usize, k: usize) {
-    kingdoms[node] = k;
-
-    for &adj in &reversed_adj_vecs[node] {
-        if kingdoms[adj] == 0 {
-            search2(reversed_adj_vecs, kingdoms, adj, k);
-        }
-    }
+struct Scc {
+    adj_vecs: Vec<Vec<usize>>,
+    reversed_adj_vecs: Vec<Vec<usize>>,
 }
 
-fn search1(
-    adj_vecs: &[Vec<usize>],
-    sorted_nodes: &mut Vec<usize>,
-    visited: &mut [bool],
-    node: usize,
-) {
-    visited[node] = true;
-
-    for &adj in &adj_vecs[node] {
-        if !visited[adj] {
-            search1(adj_vecs, sorted_nodes, visited, adj);
+#[allow(dead_code)]
+impl Scc {
+    fn new(n: usize) -> Self {
+        Self {
+            adj_vecs: vec![Vec::new(); n],
+            reversed_adj_vecs: vec![Vec::new(); n],
         }
     }
 
-    sorted_nodes.push(node);
+    fn add_edge(&mut self, from: usize, to: usize) {
+        self.adj_vecs[from].push(to);
+        self.reversed_adj_vecs[to].push(from);
+    }
+
+    fn topological_sort(&self) -> Vec<usize> {
+        let n = self.adj_vecs.len();
+
+        let mut sorted = Vec::new();
+        let mut visited = vec![false; n];
+        for i in 0..n {
+            if !visited[i] {
+                self.search1(&mut sorted, &mut visited, i);
+            }
+        }
+        sorted.reverse();
+
+        sorted
+    }
+
+    fn search1(&self, sorted: &mut Vec<usize>, visited: &mut [bool], node: usize) {
+        visited[node] = true;
+
+        for &adj in &self.adj_vecs[node] {
+            if !visited[adj] {
+                self.search1(sorted, visited, adj);
+            }
+        }
+
+        sorted.push(node);
+    }
+
+    fn build_components(&self) -> Vec<usize> {
+        let n = self.adj_vecs.len();
+
+        let sorted = self.topological_sort();
+
+        let mut components = vec![usize::MAX; n];
+        let mut component = 0;
+        for node in sorted {
+            if components[node] == usize::MAX {
+                self.search2(&mut components, component, node);
+                component += 1;
+            }
+        }
+
+        components
+    }
+
+    fn search2(&self, components: &mut [usize], component: usize, node: usize) {
+        components[node] = component;
+
+        for &adj in &self.reversed_adj_vecs[node] {
+            if components[adj] == usize::MAX {
+                self.search2(components, component, adj);
+            }
+        }
+    }
 }
