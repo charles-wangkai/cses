8af11f22e69f8993d0cdb256c7ba4f0a0f4977f7
Kai Wang <charles.wangkai@gmail.com>
2026-02-02 20:25:22 +0800

    Distinct Routes

--- src/bin/problemset_1711.rs
+++ src/bin/problemset_1711.rs
@@ -25,30 +25,17 @@ fn main() {
 }
 
 fn solve(n: usize, a: &[usize], b: &[usize]) -> String {
-    let mut edges = Vec::new();
-    let mut edge_lists = vec![Vec::new(); n];
+    let mut max_flow = MaxFlow::new(n);
     for i in 0..a.len() {
-        edges.push(Edge {
-            from: a[i] - 1,
-            to: b[i] - 1,
-            capacity: 1,
-        });
-        edge_lists[a[i] - 1].push(edges.len() - 1);
-
-        edges.push(Edge {
-            from: b[i] - 1,
-            to: a[i] - 1,
-            capacity: 0,
-        });
-        edge_lists[b[i] - 1].push(edges.len() - 1);
+        max_flow.add_edges(a[i] - 1, b[i] - 1, 1);
     }
 
-    dinic(&mut edges, &edge_lists, 0, n - 1);
+    max_flow.dinic(0, n - 1);
 
     let mut next_queues = vec![VecDeque::new(); n];
-    for i in (0..edges.len()).step_by(2) {
-        if edges[i].capacity == 0 {
-            next_queues[edges[i].from].push_back(edges[i].to);
+    for i in (0..max_flow.edges.len()).step_by(2) {
+        if max_flow.edges[i].capacity == 0 {
+            next_queues[max_flow.edges[i].from].push_back(max_flow.edges[i].to);
         }
     }
 
@@ -84,80 +71,108 @@ fn solve(n: usize, a: &[usize], b: &[usize]) -> String {
     )
 }
 
-fn dinic(edges: &mut [Edge], edge_lists: &[Vec<usize>], s: usize, t: usize) {
-    while let Some(mut levels) = bfs(edges, edge_lists, s, t) {
-        loop {
-            let minflow = dfs(edges, edge_lists, &mut levels, s, t, i32::MAX);
-            if minflow == 0 {
-                break;
+struct MaxFlow {
+    edges: Vec<Edge>,
+    edge_vecs: Vec<Vec<usize>>,
+}
+
+#[allow(dead_code)]
+impl MaxFlow {
+    fn new(size: usize) -> Self {
+        Self {
+            edges: Vec::new(),
+            edge_vecs: vec![Vec::new(); size],
+        }
+    }
+
+    fn add_edges(&mut self, u: usize, v: usize, cap: i32) {
+        self.edges.push(Edge {
+            from: u,
+            to: v,
+            capacity: cap,
+        });
+        self.edge_vecs[u].push(self.edges.len() - 1);
+
+        self.edges.push(Edge {
+            from: v,
+            to: u,
+            capacity: 0,
+        });
+        self.edge_vecs[v].push(self.edges.len() - 1);
+    }
+
+    fn dinic(&mut self, s: usize, t: usize) -> i32 {
+        let mut result = 0;
+        while let Some(mut levels) = self.bfs(s, t) {
+            loop {
+                let minflow = self.dfs(&mut levels, s, t, i32::MAX);
+                if minflow == 0 {
+                    break;
+                }
+
+                result += minflow;
             }
         }
+
+        result
+    }
+
+    fn bfs(&mut self, s: usize, t: usize) -> Option<Vec<i32>> {
+        let mut levels = vec![-1; self.edge_vecs.len()];
+        levels[s] = 0;
+
+        let mut queue = VecDeque::new();
+        queue.push_back(s);
+
+        while let Some(head) = queue.pop_front() {
+            if head == t {
+                return Some(levels);
+            }
+
+            for &e in &self.edge_vecs[head] {
+                if self.edges[e].capacity != 0 && levels[self.edges[e].to] == -1 {
+                    levels[self.edges[e].to] = levels[head] + 1;
+                    queue.push_back(self.edges[e].to);
+                }
+            }
+        }
+
+        None
+    }
+
+    fn dfs(&mut self, levels: &mut [i32], s: usize, t: usize, low: i32) -> i32 {
+        if s == t {
+            return low;
+        }
+
+        let mut result = 0;
+        for &e in &self.edge_vecs[s].iter().copied().collect::<Vec<_>>() {
+            if self.edges[e].capacity != 0 && levels[self.edges[e].to] == levels[s] + 1 {
+                let next = self.dfs(
+                    levels,
+                    self.edges[e].to,
+                    t,
+                    (low - result).min(self.edges[e].capacity),
+                );
+                self.edges[e].capacity -= next;
+                self.edges[e ^ 1].capacity += next;
+
+                result += next;
+                if result == low {
+                    break;
+                }
+            }
+        }
+
+        if result == 0 {
+            levels[s] = -1;
+        }
+
+        result
     }
 }
 
-fn bfs(edges: &[Edge], edge_lists: &[Vec<usize>], s: usize, t: usize) -> Option<Vec<i32>> {
-    let mut levels = vec![-1; edge_lists.len()];
-    levels[s] = 0;
-
-    let mut queue = VecDeque::new();
-    queue.push_back(s);
-
-    while let Some(head) = queue.pop_front() {
-        if head == t {
-            return Some(levels);
-        }
-
-        for &e in &edge_lists[head] {
-            if edges[e].capacity != 0 && levels[edges[e].to] == -1 {
-                levels[edges[e].to] = levels[head] + 1;
-                queue.push_back(edges[e].to);
-            }
-        }
-    }
-
-    None
-}
-
-fn dfs(
-    edges: &mut [Edge],
-    edge_lists: &[Vec<usize>],
-    levels: &mut [i32],
-    s: usize,
-    t: usize,
-    low: i32,
-) -> i32 {
-    if s == t {
-        return low;
-    }
-
-    let mut result = 0;
-    for &e in &edge_lists[s] {
-        if edges[e].capacity != 0 && levels[edges[e].to] == levels[s] + 1 {
-            let next = dfs(
-                edges,
-                edge_lists,
-                levels,
-                edges[e].to,
-                t,
-                (low - result).min(edges[e].capacity),
-            );
-            edges[e].capacity -= next;
-            edges[e ^ 1].capacity += next;
-
-            result += next;
-            if result == low {
-                break;
-            }
-        }
-    }
-
-    if result == 0 {
-        levels[s] = -1;
-    }
-
-    result
-}
-
+#[allow(dead_code)]
 struct Edge {
     from: usize,
     to: usize,
